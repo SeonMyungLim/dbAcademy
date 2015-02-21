00c8e87036899e4f5e9ded9bd05da48d2dd43b45
CKO <cha0713@konkuk.ac.kr>
2015-02-21 21:00:42 +0900

    todoList

--- app/js/app.js
+++ app/js/app.js
@@ -2,27 +2,43 @@
 
 /* App Module */
 
-var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatAnimations',
+var TODOApp = angular.module('TODOApp',[]);
 
-  'phonecatControllers',
-  'phonecatFilters',
-  'phonecatServices'
-]);
+var todoList = [
+    {'done' : true,  'title':"주제 고르기"},
+    {'done' : false, 'title':"작성하기"},
+    {'done' : false, 'title':"놀기"}
+    ];
 
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
+TODOApp.controller('todoCtrl', function($scope){
+     $scope.appName = 'Todo List';
+    //초기 할일 목록 설정
+
+    $scope.todoList = todoList;
+
+    //새로운 할 일 추가
+    $scope.addNewTodo = function(newTitle){
+        todoList.push({done:false, title:newTitle});
+        $scope.newTitle='';
+    };
+
+    //완료한 일 삭제
+    $scope.archive = function(){
+        for(var i=$scope.todoList.length-1; i>=0; i--){
+            if($scope.todoList[i].done){
+                $scope.todoList.splice(i,l);
+            }
+        };
+    };
+
+    //남은 할 일 수 계산
+    $scope.remain=function(){
+        var remainCount=0;
+        angular.forEach($scope.todoList, function(value, key){
+         if(value.done ==false) remainCount++;
+        })
+         return remainCount;
+    }
+});
+
+
