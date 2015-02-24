95c394e9fc67b5f7a4c271bc802a7507d66cc340
youngheon <hewillon@gmail.com>
2015-02-24 11:29:46 +0900

    Not used code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,30 +2,6 @@
 
 /* Controllers */
 
-var phonecatControllers = angular.module('phonecatControllers', []);
-
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-	$scope.left  = function() {
-        if (!$scope.message || !$scope.message.length == 0)
-            return;
-        return $scope.message.length + " / " + 300;
-    };
-  }]);
-
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
-  }]);
-
 
 TODOApp.controller('todoCtrl', function($scope){
     //todolist initializing
