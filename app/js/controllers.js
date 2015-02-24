81378db2e4e23197340caa22a0e5e900b238ab7a
DEP <arkduni@gmail.com>
2015-02-24 16:14:26 +0900

    controller function 수정

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,11 +8,6 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
   function($scope, Phone) {
     $scope.phones = Phone.query();
     $scope.orderProp = 'age';
-	$scope.left  = function() {
-        if (!$scope.message || !$scope.message.length == 0)
-            return;
-        return $scope.message.length + " / " + 300;
-    };
   }]);
 
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
